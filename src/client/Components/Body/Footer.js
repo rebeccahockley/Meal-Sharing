9a694d6e3dfbd8d184e89f1dc364be92334b16d3
rebeccahockley <hockley.r@yahoo.com>
2022-06-13 11:11:54 +0200

    Links added to footer

--- src/client/Components/Body/Footer.js
+++ src/client/Components/Body/Footer.js
@@ -6,26 +6,34 @@ export const Footer = () => {
     <footer>
       <div className="lineDecoration"></div>
       <div id="footerImageContainer">
-        <img
-          src="./src/client/Components/Assets/Facebook.jpg"
-          className="footerImages"
-          alt="Outline of Facebook Logo"
-        ></img>
-        <img
-          src="./src/client/Components/Assets/GitHub.jpg"
-          className="footerImages"
-          alt="Outline of GitHub Logo"
-        ></img>
-        <img
-          src="./src/client/Components/Assets/Instagram.png"
-          className="footerImages"
-          alt="Outline of Instagram Logo"
-        ></img>
-        <img
-          src="./src/client/Components/Assets/LinkedIn.png"
-          className="footerImages"
-          alt="Outline of LinkedIn Logo"
-        ></img>
+        <a href="https://www.facebook.com/">
+          <img
+            src="./src/client/Components/Assets/Facebook.jpg"
+            className="footerImages"
+            alt="Outline of Facebook Logo"
+          ></img>
+        </a>
+        <a href="https://github.com/rebeccahockley">
+          <img
+            src="./src/client/Components/Assets/GitHub.jpg"
+            className="footerImages"
+            alt="Outline of GitHub Logo"
+          ></img>
+        </a>
+        <a href="https://www.instagram.com/">
+          <img
+            src="./src/client/Components/Assets/Instagram.png"
+            className="footerImages"
+            alt="Outline of Instagram Logo"
+          ></img>
+        </a>
+        <a href="https://www.linkedin.com/in/rebecca-hockley-6b4475238/">
+          <img
+            src="./src/client/Components/Assets/LinkedIn.png"
+            className="footerImages"
+            alt="Outline of LinkedIn Logo"
+          ></img>
+        </a>
       </div>
     </footer>
   );
