350feb9e1640106aa82b7386ed86c55dbc39aaca
rebeccahockley <hockley.r@yahoo.com>
2022-06-15 22:58:08 +0200

    WiP

--- src/client/Components/Body/Footer.js
+++ src/client/Components/Body/Footer.js
@@ -8,28 +8,28 @@ export const Footer = () => {
       <div id="footerImageContainer">
         <a href="https://www.facebook.com/">
           <img
-            src="./src/client/Components/Assets/Facebook.jpg"
+            src="Assets/Facebook.jpg"
             className="footerImages"
             alt="Outline of Facebook Logo"
           ></img>
         </a>
         <a href="https://github.com/rebeccahockley">
           <img
-            src="./src/client/Components/Assets/GitHub.jpg"
+            src="Assets/GitHub.jpg"
             className="footerImages"
             alt="Outline of GitHub Logo"
           ></img>
         </a>
         <a href="https://www.instagram.com/">
           <img
-            src="./src/client/Components/Assets/Instagram.png"
+            src="Assets/Instagram.png"
             className="footerImages"
             alt="Outline of Instagram Logo"
           ></img>
         </a>
         <a href="https://www.linkedin.com/in/rebecca-hockley-6b4475238/">
           <img
-            src="./src/client/Components/Assets/LinkedIn.png"
+            src="Assets/LinkedIn.png"
             className="footerImages"
             alt="Outline of LinkedIn Logo"
           ></img>
